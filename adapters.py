5288aa72fadc45fc8b07254128afb9ab5d8a8ac9
Arisha-27 <arisharizwan786@gmail.com>
2026-02-26 19:38:25 +0530

    refactor: update response model and align repository data

--- adapters.py
+++ adapters.py
@@ -6,8 +6,8 @@ from ports import UserRepositoryPort
 class InMemoryUserRepository(UserRepositoryPort):
     def __init__(self):
         self.users = {
-            1: User(id=1, name="Rippling", role="Admin"),
-            2: User(id=2, name="Arisha", role="Developer")
+            1: User(id=1, name="Arisha", role="RippIntern"),
+            2: User(id=2, name="Rippling", role="Admin")
         }
 
     def get_user_by_id(self, user_id: int) -> Optional[User]:
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from adapters import InMemoryUserRepository
 from use_cases import UserUseCase
 
@@ -7,19 +8,26 @@ app = FastAPI()
 user_repo = InMemoryUserRepository()
 user_service = UserUseCase(user_repo)
 
-# Primary Adapter: REST API Route
-@app.get("/users/{user_id}")
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    role: str
+
+class GetUserResponse(BaseModel):
+    status: str
+    data: UserResponse
+
+
+@app.get("/users/{user_id}", response_model=GetUserResponse)
 def get_user(user_id: int):
     try:
         user = user_service.get_user_info(user_id)
     except ValueError as e:
-        # Catch the bad input and return a 400 error
         raise HTTPException(status_code=400, detail=str(e))
-        
+
     if not user:
-        # If it's valid but just not in the DB, return 404
         raise HTTPException(status_code=404, detail="User not found")
-        
+
     return {
         "status": "success",
         "data": user
